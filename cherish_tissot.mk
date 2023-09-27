68661f896dd9b186fcaf17e7731a3f659c4e90cf
Lafactorial <hknhzl@gmail.com>
2023-09-27 19:00:00 +0300

    CherishOS bring up

--- cherish_tissot.mk
+++ cherish_tissot.mk
@@ -9,40 +9,32 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := lineage_tissot
+PRODUCT_NAME := cherish_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
 PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
-PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
-
-# LineageOS Stuffs
+# CherishOS  Stuffs
+CHERISH_BUILD_TYPE := UNOFFICIAL
+CHERISH_VANILLA := true
 TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GOOGLE_APEX := false
 TARGET_SUPPORTS_QUICK_TAP := true
 
-# Sushi Bootanimation
-SUSHI_BOOTANIMATION := 1080
+# Maintainer
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.cherish.maintainer=HaKaN \
 
-# Maintainer flag
-RICE_MAINTAINER := HaKaN
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Device flag
-RICE_DEVICE := tissot
-
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
